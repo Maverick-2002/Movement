5424cbc09d180125b205d91bf6d891aa7b7773de
Maverick-2002 <131183332+Maverick-2002@users.noreply.github.com>
2024-02-01 10:30:22 +0530

    commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed;
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movement*speed*Time.deltaTime);
+    }
+
+    public void OnMovement(InputValue val)
+    {
+        SetMovement(val.Get<Vector2>());
+    }
+    public void OnMovementEvent(InputAction.CallbackContext ctx)
+    {
+        SetMovement(ctx.ReadValue<Vector2>());
+    }
+    private void SetMovement(Vector2 moveVal)
+    {
+        movement = moveVal;
+        movement.z = movement.y;
+        movement.y = 0;
+
+    }
+}
