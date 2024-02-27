8e8b2d90c2f3fd10acbf77b6fb91820b9f27f29c
Maverick-2002 <131183332+Maverick-2002@users.noreply.github.com>
2024-02-27 23:23:35 +0530

    Final Commit

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
 
 public class Trigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Working Trigger");
         if (other.gameObject.CompareTag("Portal"))
         {
-            //Addressables.LoadAssetAsync<GameObject>("Room").Completed += OnRoomLoaded;
+            Addressables.LoadAssetAsync<GameObject>("Room").Completed += OnRoomLoaded;
             Debug.Log("work");
         }
         else if (other.gameObject.CompareTag("SceneTrigger"))
         {
-           // Addressables.LoadSceneAsync("Level1").Completed += OnLevelLoaded;
+           Addressables.LoadSceneAsync("Level1").Completed += OnLevelLoaded;
         }
     }
     private void OnLevelLoaded(AsyncOperationHandle<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance> obj2)
@@ -27,6 +29,6 @@ public class Trigger : MonoBehaviour
     {
         Debug.Log(obj3.Result.name);
         GameObject player = Instantiate(obj3.Result);
-        player.transform.position = new Vector3(1.7294f, 3.296516f, -0.6211069f);
+        player.transform.position = new Vector3(-11.522f, -14.22f, 11.58f);
     }
 }
